336db6c955d7925fd5a87a8e75d9dcc88a60cf05
Daniel Plotkin <116699460+danplotkin@users.noreply.github.com>
2023-11-24 12:58:26 -0500

    Initial commit

---
+++ scripts/validate_openai_apikey.py
@@ -0,0 +1,25 @@
+from openai import OpenAI
+import os  
+from dotenv import load_dotenv
+
+load_dotenv()
+  
+# Set the OpenAI API key from the environment variable  
+OpenAI.api_key = os.environ["OPENAI_API_KEY"]  
+
+
+# Define client
+client = OpenAI()
+  
+# Test the API key by retrieving the list of models  
+try:
+    completion = client.completions.create(
+    model="gpt-3.5-turbo-instruct",
+    prompt="Say this validation test was successful",
+    max_tokens=7,
+    temperature=0
+    )
+
+    print(completion.choices[0].text)
+except Exception as e:
+    print(f'Failed with the following error messge: {e}')
